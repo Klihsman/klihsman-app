e1d6a35cc9c413ca2b7928e84e00dfc1869ffaef
Klihsman Freitas <klihsman@gmail.com>
2024-11-15 22:38:15 -0300

    init

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,25 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import './App.scss';
+import profileImage from './assets/profile-image.png';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="main-container">
+        <div className='info-container'></div>
+        <div className="profile-container">
+          <img
+            src={profileImage}
+            className="profile-image"
+            alt="profile-image"
+          />
+          <span className="profile-user-name">Klihsman Freitas</span>
+          <div className="profile-tags">
+            <span className="profile-tag">#React</span>
+            <span className="profile-tag">#Angular</span>
+            <span className="profile-tag">#Typescript</span>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
